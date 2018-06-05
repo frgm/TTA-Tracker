597153d14bd241f80dded116952f05bccce5cedb
FRGM <frgm88@gmail.com>
2018-06-04 22:02:22 -0400

    Startup routine

--- trackerMain/apps.py
+++ trackerMain/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+from subprocess import call
 
 
 class TrackermainConfig(AppConfig):
     name = 'trackerMain'
+    def ready(self):
+        call('multichain-util -datadir="%APPDATA%\MultiChain-Host" create TTA-Tracker') #Error if it exists
+        call('multichaind TTA-Tracker -daemon -datadir="%APPDATA%\MultiChain-Host"')
+        
+        rpcUser = 'multichainrpc'
+        rpcPassword = 'At5XJMFjjfCZhwfhEctA3XoY5aMKN1ZwivajyYWT163R'
+        rpcHost = 'localhost'
+        rpcPort = '9258 '
+        chainname = 'test'
+        global mcapi = AuthServiceProxy('http://%s:%s@%s:%s'%(rpcUser,rpcPassword,rpcHost,rpcPort))
+        endAddress = 'end' #Burn address. Change to actual address
+        mcapi.grant(addr,"connect,send,receive,create,issue,mine")
+        return
--- trackerMain/views.py
+++ trackerMain/views.py
@@ -5,13 +5,7 @@ import trackerMain.models as md
 import sys
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
-rpcUser = 'multichainrpc'
-rpcPassword = 'At5XJMFjjfCZhwfhEctA3XoY5aMKN1ZwivajyYWT163R'
-rpcHost = 'localhost'
-rpcPort = '9258 '
-chainname = 'test'
-mcapi = AuthServiceProxy('http://%s:%s@%s:%s'%(rpcUser,rpcPassword,rpcHost,rpcPort))
-endAddress = 'end' #Burn address. Change to actual address
+#mcapi declared in appconfig
 
 def index(request):
     return HttpResponse("Test")
